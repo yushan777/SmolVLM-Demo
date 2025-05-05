945ee8ac76285dcea34efb4a18a28159fc456e11
yushan777 <123392471+yushan777@users.noreply.github.com>
2025-05-05 21:12:21 +0100

    Update main.py
    ...

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ messages = [
         "role": "user",
         "content": [
             {"type": "image"},
-            {"type": "text", "text": "caption this image."}
+            {"type": "text", "text": "caption this image into a single paragraph."}
         ]
     },
 ]
